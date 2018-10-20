bd49fedcf5a8bb88d00e5c0afd4159e5de2c3497
kraftwerk28 <kefirchik3@gmail.com>
2018-10-20 15:00:34 +0300

    Some changes

--- my-stuff.js
+++ my-stuff.js
@@ -1,8 +1,13 @@
 import Ph from 'phaser';
 
+function outerPreload() {
+  // console.log(game);
+  this.load.image('money', 'assets/money-frame.png');
+}
+
 function outerCreate() {
-
-
+  this.add.image(config.width - 340 + 340 / 2, 125 / 2, 'money');
+  player.setBounce(1);
 }
 
 function outerUpdate() {
@@ -13,4 +18,4 @@ function outerRender() {
 
 }
 
-export { outerCreate, outerUpdate, outerRender };
+export { outerCreate, outerUpdate, outerRender, outerPreload };
