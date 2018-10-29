331302240dac568ef63cb01688fd7fd3badad63c
kraftwerk28 <kefirchik3@gmail.com>
2018-10-29 20:44:14 +0200

    BITCH

--- dist/antiexit.js
+++ dist/antiexit.js
@@ -1,7 +1,7 @@
 var ww = document.getElementsByTagName('iframe')[0].contentWindow;
 
 function restrict() {
-  if (ww.gameStarted && !confirm('Do not leave the page! We will give you 300 gold!')) {
+  if (ww.gameStarted && !confirm('Do not leave the page! We will give you 150 gold!')) {
     location.replace('index.html');
   } else {
     ww.cll300 = true;
